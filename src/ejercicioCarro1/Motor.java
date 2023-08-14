5755096396bb3818c0c74a61bd875c46fe7f5e33
nicolas <nicolasmahecha1125@gmail.com>
2023-08-14 00:21:16 -0500

    primera vez

--- src/ejercicioCarro1/Motor.java
+++ src/ejercicioCarro1/Motor.java
@@ -1,6 +1,6 @@
 package ejercicioCarro1;
 
-public class Motor{
+public class Motor {
     private String marca;
     private String nombreValvula;
     public  Motor(String marca, String nombreValvula){
